54bfdd4659050bb6d3f6123e423c70e0513869d4
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2023-03-24 21:18:47 +0530

    dp

--- DP/dp on stocks/Best Time to Buy and Sell Stock II.cpp
+++ DP/dp on stocks/Best Time to Buy and Sell Stock II.cpp
@@ -15,6 +15,27 @@ long f(int ind, int buy, long *values, int n, vector<vector<long>>& dp) {
 } 
 long getMaximumProfit(long *values, int n)
 {
-    vector<vector<long>>dp(n,vector<long>(2,-1));
-    return f(0,1,values,n,dp);
+    vector<vector<long>>dp(n+1,vector<long>(2,-1));
+    // return f(0,1,values,n,dp);
+    
+    //base condition
+    dp[n][0] = dp[n][1] = 0;
+    
+    long profit;
+    
+    for(int ind= n-1; ind>=0; ind--){
+        for(int buy=0; buy<=1; buy++){
+            if(buy==0){// We can buy the stock
+                profit = max(0+dp[ind+1][0], -values[ind] + dp[ind+1][1]);
+            }
+    
+            if(buy==1){// We can sell the stock
+                profit = max(0+dp[ind+1][1], values[ind] + dp[ind+1][0]);
+            }
+            
+            dp[ind][buy]  = profit;
+        }
+    }
+    return dp[0][0];
+
 }
