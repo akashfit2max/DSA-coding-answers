567622c83d46ad74cea15712d9d015eb6d0fcb52
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2023-04-08 22:39:01 +0530

    dp

---
+++ DP/partition dp/Boolean Evaluation.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+int mod = 1000000007;
+
+int f(int i, int j, int isTrue, string &exp, vector<vector<vector<ll>>> &dp) {
+    //Base case 1:
+    if (i > j) return 0;
+    //Base case 2:
+    if (i == j) {
+        if (isTrue == 1) return exp[i] == 'T';
+        else return exp[i] == 'F';
+    }
+
+    if (dp[i][j][isTrue] != -1) return dp[i][j][isTrue];
+    ll ways = 0;
+    for (int ind = i + 1; ind <= j - 1; ind += 2) {
+        ll lT = f(i, ind - 1, 1, exp, dp);
+        ll lF = f(i, ind - 1, 0, exp, dp);
+        ll rT = f(ind + 1, j, 1, exp, dp);
+        ll rF = f(ind + 1, j, 0, exp, dp);
+
+        if (exp[ind] == '&') {
+            if (isTrue) ways = (ways + (lT * rT) % mod) % mod;
+            else ways = (ways + (lF * rT) % mod + (lT * rF) % mod + (lF * rF) % mod) % mod;
+        }
+        else if (exp[ind] == '|') {
+            if (isTrue) ways = (ways + (lF * rT) % mod + (lT * rF) % mod + (lT * rT) % mod) % mod;
+            else ways = (ways + (lF * rF) % mod) % mod;
+        }
+        else {
+            if (isTrue) ways = (ways + (lF * rT) % mod + (lT * rF) % mod) % mod;
+            else ways = (ways + (lF * rF) % mod + (lT * rT) % mod) % mod;
+        }
+    }
+    return dp[i][j][isTrue] = ways;
+}
+int evaluateExp(string & exp) {
+    // Write your code here.
+    int n = exp.size();
+    // vector<vector<vector<ll>>> dp(n, vector<vector<ll>>(n, vector<ll>(2, -1)));
+    // return f(0, n - 1, 1, exp, dp);
+    vector<vector<vector<ll>>> dp(n, vector<vector<ll>>(n, vector<ll>(2, 0)));
+    for(int i=n-1; i>=0; i--) {
+        for(int j=i; j<n; j++) {
+            if(i > j) continue;
+            if(i == j) {
+                dp[i][j][0] = (exp[i] == 'F');
+                dp[i][j][1] = (exp[i] == 'T');
+            }
+            else {
+                ll ways = 0;
+                for(ll ind=i+1;ind<=j-1;ind+=2)
+                {
+                    ll lt=dp[i][ind-1][1]%mod;
+                    ll rt=dp[ind+1][j][1]%mod;
+                    ll lf=dp[i][ind-1][0]%mod;
+                    ll rf=dp[ind+1][j][0]%mod;
+                    if(exp[ind]=='&')
+                    {
+                        dp[i][j][0]=(dp[i][j][0]+(lt*rf+lf*rt+lf*rf))%mod;
+                        dp[i][j][1]=(dp[i][j][1]+(lt*rt))%mod;
+                    }
+                    else if(exp[ind]=='|')
+                    {
+                        dp[i][j][0]=(dp[i][j][0]+(lf*rf))%mod;
+                        dp[i][j][1]=(dp[i][j][1]+(lt*rf+lf*rt+lt*rt))%mod;
+                    }
+                    else
+                    {
+                        dp[i][j][0]=(dp[i][j][0]+(lt*rt+lf*rf))%mod;
+                        dp[i][j][1]=(dp[i][j][1]+(lt*rf+lf*rt))%mod;
+                    }
+                }
+            }
+        }
+    }
+    return dp[0][n-1][1];
+}
