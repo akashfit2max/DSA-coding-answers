e93a7fc1c6cfe651f90b1bf18f1b7b49fc568ad7
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2023-04-11 22:44:36 +0530

    string

--- Strings/leetcode/15.Zigzag Conversion.cpp
+++ Strings/leetcode/15.Zigzag Conversion.cpp
@@ -19,3 +19,35 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1) return s;
+        vector<string>ans(numRows);
+        bool flag = false;
+        int i = 0;
+
+        for(auto ch : s) {
+            ans[i] += ch;
+            if(i==0 || i == numRows-1) {
+                flag = !flag;
+            }
+            if(flag) {
+                i++;
+            }
+            else {
+                i--;
+            }
+        }
+        string zigzag = "";
+        for(auto str : ans) {
+            zigzag += str;
+        }
+        return zigzag;
+    }
+};
