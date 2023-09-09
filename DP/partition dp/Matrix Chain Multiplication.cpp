1f068299a270fbfbab62cdf33c8024a2dbbf85ac
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2023-09-09 08:22:03 +0530

    update

--- DP/partition dp/Matrix Chain Multiplication.cpp
+++ DP/partition dp/Matrix Chain Multiplication.cpp
@@ -16,7 +16,7 @@ int matrixMultiplication(vector<int> &arr, int N)
     vector<vector<int>>dp(N,vector<int>(N,0));
     for(int i=1; i<N; i++) {
         dp[i][i] = 0;
-    }
+    }   
     for(int i=N-1; i>=1; i--) {
         for(int j=i+1; j<N; j++) {
             if(i == j) continue;
