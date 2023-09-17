049525ccefd919d84ad8b066d77c577210b7cb23
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2023-09-17 13:54:00 +0530

    update

--- recusion/recursion leetcode/15.word search.cpp
+++ recusion/recursion leetcode/15.word search.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        for(int i=0;i<board.size();i++) {
-            for(int j=0;j<board[0].size();j++) {
-                if(board[i][j] == word[0] && dfs(board,i,j,0,word)) {
-                    return true;
-                } 
+    
+    int dr[4]={0,1,0,-1};
+    int dc[4]={1,0,-1,0};
+    int vis[7][7];
+    bool f(int i, int j, vector<vector<char>>&board , string word,int index){
+        int m = board.size();
+        int n = board[0].size();
+        if(index == word.size()) return true;
+        for(int k=0;k<4;k++){
+            int nr = dr[k]+i;
+            int nc = dc[k]+j;
+            if(nr>=0 && nc>=0 && nr<m && nc<n){
+                if(board[nr][nc] == word[index]){
+                      if(!vis[nr][nc]){
+                      vis[nr][nc]=1;
+                      if(f(nr,nc,board,word,index+1)) return true;
+                      vis[nr][nc]=0;
+                      }
+                }
             }
         }
+
         return false;
     }
-    
-    bool dfs(vector<vector<char>>& board, int i, int j, int count, string word) {
-        if(count==word.length()) {
-            return true;
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        memset(vis,0,sizeof(vis));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[0]){
+                    vis[i][j]=1;
+                    if(f(i,j,board,word,1)) return true;
+                    vis[i][j]=0;
+                }
+            }
         }
-        
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[count] ) {
-            return false;
-        }
-        
-        char temp = board[i][j];
-        board[i][j] = '*';
-        
-        bool found = dfs(board,i+1,j,count+1,word) || dfs(board,i-1,j,count+1,word) ||
-                    dfs(board,i,j+1,count+1,word) || dfs(board,i,j-1,count+1,word);
-        
-        board[i][j] = temp;
-        
-        return found;
+
+        return false;
+     
+
     }
-    
 };
