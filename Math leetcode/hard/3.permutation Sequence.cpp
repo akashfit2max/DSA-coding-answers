4fe862768e113fe2fcdd80e05a63d3ae49489053
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2023-10-11 08:14:31 +0530

    update

--- Math leetcode/hard/3.permutation Sequence.cpp
+++ Math leetcode/hard/3.permutation Sequence.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        //let n=4 ,k=17
-        //step1-> possible numbers{1,2,3,4}
-        //factorial=6
-        vector<int> numbers;
-        int fact=1;
-        for(int i=1;i<=n;i++)
-        {
-            fact=fact*i;
-            numbers.push_back(i);
+        int fact = 1;
+        vector<int>nums;
+        // n = 4 calculate 3! 
+        for(int i=1; i<n; i++) {
+            fact *= i;
+            nums.push_back(i);
         }
-        k=k-1 ; //zero based indexing
-        fact=fact/n; //if size=n we want factorial=(n-1)!
-        string res="";
-        while(true)
-        {                               //idx->0,1,2,3   
-            res+=to_string(numbers[k/fact]); //[1,2,3,4] index_no=16/6 ->2 
-            numbers.erase(numbers.begin()+(k/fact));
-            if(numbers.size()==0)
-                break;
-            k=k%fact;       //sequence number=16%6 -> 4
-            fact=fact/numbers.size();
+        nums.push_back(n);
+        k = k-1;
+        string ans = "";
+        while(true) {
+            ans += to_string(nums[k/fact]);
+            nums.erase(nums.begin() + k/fact);
+            if(nums.size() == 0) break;
+            k = k%fact;
+            fact = fact / nums.size();
         }
-        return res;
+        return ans;
     }
 };
