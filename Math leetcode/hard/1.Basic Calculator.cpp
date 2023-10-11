870c92d34348ed8bbbff7f19b40694b777c6f290
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2023-10-11 08:12:02 +0530

    update

--- Math leetcode/hard/1.Basic Calculator.cpp
+++ Math leetcode/hard/1.Basic Calculator.cpp
@@ -1,54 +1,46 @@
 class Solution {
 public:
     int calculate(string s) {
-        int l = s.length();
         int sign = 1;
-        int ans = 0;
-        long long int currNo = 0;
+        int sum = 0;
         stack<int>st;
-        
-        for(int i=0;i<l;i++)
-        {
-            if(isdigit(s[i]))
-            {
-                currNo = s[i]-'0';
-                while(i+1 < l && isdigit(s[i+1]))
-                {
-                    currNo = currNo*10 + s[i+1]-'0';
+        int n = s.length();
+
+        for(int i=0; i<n; i++) {
+            
+            // if its a digit
+            if(isdigit(s[i])) {
+                int num = 0;
+                // more than one digit
+                while(s.length() > i && isdigit(s[i])) {
+                    num =num*10 + (s[i]-'0');
                     i++;
                 }
-                cout<<currNo;
-                currNo = currNo*sign;
-                ans+=currNo;
-                sign=1;
-                currNo=0;
+                sum += num*sign;
+                i--;
             }
-            else if(s[i]=='-')
-            {
-                sign=-1;
+
+            else if(s[i] == '+') {
+                sign = 1;
             }
-            else if(s[i]=='+')
-            {
-                sign=1;
+
+            else if(s[i] == '-') {
+                sign = -1;
             }
-            else if(s[i] == '(')
-            {
-                st.push(ans);
+            // push into stack the sum and the sign
+            else if(s[i] == '(') {
+                st.push(sum);
                 st.push(sign);
-                ans=0;
-                sign=1;
+                sum = 0;
+                sign = 1;
             }
-            else if(s[i]==')')
-            {
-                int prevSign = st.top();
+            else if(s[i] == ')') {
+                sum = st.top()*sum;
                 st.pop();
-                ans = ans*prevSign;
-                int prevNo = st.top();
+                sum += st.top();
                 st.pop();
-                ans = ans+prevNo;
             }
         }
-        
-        return ans;
+        return sum;
     }
 };
