43944f40e6eb27f63dac23615a3e3a4367915afa
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2023-11-17 21:51:17 +0530

    code update

--- Strings/leetcode/8.Longest Palindromic Substring.cpp
+++ Strings/leetcode/8.Longest Palindromic Substring.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
+    int t[1001][1001];
+    
+    bool solve(string &s, int l, int r){
+        if(l >= r) 
+            return 1;
+
+        if(t[l][r] != -1){
+            return t[l][r];
+        }
+
+        if(s[l] == s[r]) {
+            return t[l][r] = solve(s, l+1, r-1);
+        }
+
+        return t[l][r] = false;
+    }
+    
     string longestPalindrome(string s) {
         int n = s.length();
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        int maxlen=0;
-        string ans="";
-        // traverse diagonally
-        for(int dif=0;dif<n;dif++)
-        {
-            for(int i=0,j=i+dif; j<n; i++,j++)
-            {
-                if(i==j)
-                {
-                    //length 1 always palindrome
-                    dp[i][j]=1;
-                }
-                else if(dif==1)
-                {
-                    dp[i][j] = (s[i]==s[j]) ? 1 : 0;
-                }
-                else
-                {
-                    if(s[i]==s[j] && dp[i+1][j-1])
-                    {
-                        dp[i][j]= 1;
+        
+        int maxlen = INT_MIN;
+        int startingIndex = 0;
+
+        memset(t, -1, sizeof(t));
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                
+                if(solve(s, i, j)) {
+                    if(j-i+1 > maxlen){
+                        startingIndex = i;
+                        maxlen = j-i+1;
                     }
                 }
                 
-                if(dp[i][j])
-                {
-                    maxlen = max(maxlen,j-i+1);
-                    ans = s.substr(i,maxlen);
-                }
             }
         }
-        return ans;
+
+        return s.substr(startingIndex, maxlen);
     }
 };
